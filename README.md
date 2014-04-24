caac37d0380dbf4ad2b2bdfd0a2de02b986bc3fe
Christian César Robledo López <osiris2012@msn.com>
2014-04-23 23:36:33 -0500

    Created

---
+++ README.md
@@ -0,0 +1,22 @@
+simpleAccordion.js
+=================
+
+Less than 1kb jQuery plugin for transform content into tabs.
+
+## Demo
+http://masquerade-circus.creaken.com/Plugins/simpleTabs/
+
+##How to use:
+Call the plugin on the parent of the elements to make them act like tabs. 
+Each pair of children elements will act as tab and content.
+				
+##Options:
+```javascript
+	$(selector).simpleTabs(function);
+```
+The function will be called each time the user hits a tab.
+
+
+# Legal
+Author & copyright (c) 2013: [Masquerade Circus](http://masquerade-circus.creaken.com)
+Dual [MIT](http://opensource.org/licenses/MIT) & [GPLv2](http://opensource.org/licenses/GPL-2.0) license
---
+++ simpleTabs.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+	$.fn.simpleTabs = function( fn ) {
+
+			return this.each(function(){
+				var a = $(this), t = a.children().filter( ":even" ), tc = a.children().filter( ":odd" ), w = Math.floor(100 / t.length), n = null;
+				
+				a.css('position') == 'static' ? a.css('position','relative') : n;
+				
+				tc.addClass('simpleTabContainer');
+				
+				t.css({'position':'absolute','width': w + '%', 'top': '0', 'cursor' : 'pointer'}).each(function(i){
+					$(this).css('left', (w * i) + '%');
+				}).on('click', function(){
+					tc.css('display', 'none');
+					t.removeClass('simpleTabActive');
+					$(this).addClass('simpleTabActive').next().css('display', 'block');
+					/^f/.test(typeof fn) ? fn.call($(this)) : n;
+				}).addClass('simpleTab').eq(0).trigger('click');
+				
+				a.css({'padding-top': t.eq(0).outerHeight() + 1 + 'px'});
+				
+			});
+	};
+	
+})(jQuery);
